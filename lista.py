e1f9cbf0901098666a41f4dcb1a50357c7fff703
Massudesu <nicolasmassuda01@gmail.com>
2024-08-28 14:30:29 -0300

    prime

---
+++ lista.py
@@ -0,0 +1,5 @@
+lista = []
+a = [input("Coloque a informação: ")]
+a2 = [input("segunda informação: ")]
+a.extend (a2)
+print(a)
--- rota.py
+++ rota.py
@@ -11,27 +11,33 @@
 n = int(input("Quantas rotas?  "))
 
 rd = 0
-trd = 0
+trd = float('inf')
 rr = 0
-trr = float
+trr = float('inf')
+mp = []
 
 for i in range(n):
     print(f"Rota {i+1}:")
     t = int(input("Quantos trechos nesta rota? "))
     tr = 0
+    dt = 0
 
     for j in range(t):
         d = float(input(f"Distância do trecho {j+1} (km)? "))
         vm = float(input(f"Velocidade média do trecho {j+1} (km/h)? "))
         tr += d / vm
+        dt += d
+    vmp = dt / tr
+    mp.append(vmp)
 
-    if tr > trd:
-        trr = tr
+    if tr < trd:  
+        trd = tr
         rd = i + 1
 
-    if tr < trr:
-        trr = tr
-        rr = i + 1
-
-print("A rota mais demorada é a rota " + str(rd) + " com um tempo de " + str(trd) + " horas.")
-print("A rota mais rápida é a rota " + str(rr) + " com um tempo de " + str(trr) + " horas.")
+if rd > 0:
+    print(f"A rota mais rápida é a rota {rd}")
+else:
+    print("Não foi possível determinar a rota mais rápida.")
+for i in range(len(mp)):
+    vmp = mp[i]
+    print(f"A média ponderada da rota {i+1} é {vmp} km/h")
